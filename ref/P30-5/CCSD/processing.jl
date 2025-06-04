77d8fe1f6f6d9f770b672721aff8cd71c8d2c0d2
Juan Felipe Huan Lew Yee <Juan Felipe Huan Lew Yee>
2025-06-04 15:49:03 +0200

    Added reference files for CCSD(T)

---
+++ ref/P30-5/CCSD/processing.jl
@@ -0,0 +1,48 @@
+using YAML
+using Printf
+
+template = """
+memory 20Gb
+
+molecule{
+xyz}
+
+set reference rhf
+
+energy(\"CCSD(T)/def2-QZVP\")
+"""
+
+data = YAML.load_file("Reactions.yaml")
+
+for (reaction, reaction_data) in data
+    println(reaction)
+    set_name, reaction_id = split(reaction, ":")
+    molecules = reaction_data[2:end]
+    for (coeff, xyzfile) in molecules
+        println(xyzfile)
+        xyz = ""
+	mult = 0
+        for (i, line) in enumerate(eachline(xyzfile))
+            if (i==2)
+		charge, mult = split(line)
+		mult = parse(Int64,mult)
+                xyz = xyz * line * "\n"
+            elseif (i>=2)
+                atom, x, y, z = split(line)
+		atom = uppercasefirst(atom)
+		formatted_line = atom * "     " * x * "  " * y * "  " * z 
+                xyz = xyz * formatted_line * "\n"
+            end
+        end
+
+        molecule_name = xyzfile[1:(end-4)]
+        open(molecule_name*".in", "w") do fmol
+            content = replace(template, "xyz" => xyz)
+            content = replace(content, "rrrr" => molecule_name)
+	    if mult > 1
+                content = replace(content, "rhf" => "rohf")
+            end
+            print(fmol, content)
+        end
+    end
+end
