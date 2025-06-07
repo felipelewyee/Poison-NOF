b0ce094b0f6c347e48a644704960f6125d074891
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2025-06-07 19:40:52 +0200

    Added files of cc-pVTZ

---
+++ ncwomax/P30-5/GNOFmodulated20_cctz/W4-11_135_cloo.jl
@@ -0,0 +1,27 @@
+using DoNOF
+
+mol = """
+0 2
+Cl     -0.560974  -0.994637  0.000000
+O     0.000000  0.958712  0.000000
+O     1.192069  1.154892  0.000000
+"""
+
+bset,p = DoNOF.molecule(mol,"cc-pvtz",spherical=true)
+
+p.title = "W4-11_135_cloo"
+
+p.ipnof = 9
+
+p.RI = true
+p.maxit = 40
+
+p.maxloop = 10
+
+#DoNOF.set_ncwo(p,1)
+p.h_cut = 0.020*sqrt(2)
+
+C = DoNOF.read_C(title=p.title)
+n = DoNOF.read_n(title=p.title)
+
+DoNOF.energy(bset,p,C=C,n=n,do_hfidr=false,do_m_diagnostic=true)
